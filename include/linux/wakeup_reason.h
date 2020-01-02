54ee487b2633eaaed63ec47d8b40decf2180e48b
celtare21 <celtare21@gmail.com>
2020-01-02 22:00:42 +0000

    kernel: power: Only compile wakeup_reason when debugging
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: shygosh <mossynine@gmail.com>

--- include/linux/wakeup_reason.h
+++ include/linux/wakeup_reason.h
@@ -20,10 +20,9 @@
 
 #define MAX_SUSPEND_ABORT_LEN 256
 
+#if defined(CONFIG_SUSPEND) && defined (CONFIG_PM_DEBUG)
 void log_wakeup_reason(int irq);
 int check_wakeup_reason(int irq);
-
-#ifdef CONFIG_SUSPEND
 void log_suspend_abort_reason(const char *fmt, ...);
 #else
 static inline void log_suspend_abort_reason(const char *fmt, ...) { }
