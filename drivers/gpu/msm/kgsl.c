9eebf3ca7e5a3e3ea0ba3d94d172d3d9d9bfb995
Puranam V G Tejaswi <quic_pvgtejas@quicinc.com>
2022-12-20 19:24:11 +0530

    msm: kgsl: Use kthread instead of workqueue for event work
    Currently a workqueue is being used to process the event work. In certain
    scenarios like when most of CPU cores are busy, there can be a significant
    delay between the actual timestamp retire event and when the work is
    processed by the events workqueue as workqueues cannot have RT priority.
    Hence use kthread instead of workqueue for event work.
    
    Change-Id: Ib1ec7fa1ec3a133d03104c9a029dcc4c06180609
    Signed-off-by: Puranam V G Tejaswi <quic_pvgtejas@quicinc.com>
    [PA: Ported to 5.10, replaced sched_set_fifo with sched_setscheduler_nocheck]
    [dereference23: Backport to msm-4.14]
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -671,11 +671,11 @@ int kgsl_context_init(struct kgsl_device_private *dev_priv,
 	if (id == -ENOSPC) {
 		/*
 		 * Before declaring that there are no contexts left try
-		 * flushing the event workqueue just in case there are
+		 * flushing the event worker just in case there are
 		 * detached contexts waiting to finish
 		 */
 
-		flush_workqueue(device->events_wq);
+		kthread_flush_worker(device->events_worker);
 		id = _kgsl_get_context_id(device);
 	}
 
@@ -5037,6 +5037,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 {
 	int status = -EINVAL;
 	struct resource *res;
+	struct sched_param param = { .sched_priority = MAX_RT_PRIO / 2 };
 
 	status = _register_device(device);
 	if (status)
@@ -5160,8 +5161,8 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 				PM_QOS_CPU_DMA_LATENCY,
 				PM_QOS_DEFAULT_VALUE);
 
-	device->events_wq = alloc_workqueue("kgsl-events",
-		WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_SYSFS, 0);
+	device->events_worker = kthread_create_worker(0, "kgsl-events");
+	sched_setscheduler_nocheck(device->events_worker->task, SCHED_FIFO, &param);
 
 	/* Initialize the snapshot engine */
 	kgsl_device_snapshot_init(device);
@@ -5184,7 +5185,7 @@ EXPORT_SYMBOL(kgsl_device_platform_probe);
 
 void kgsl_device_platform_remove(struct kgsl_device *device)
 {
-	destroy_workqueue(device->events_wq);
+	kthread_destroy_worker(device->events_worker);
 
 	kgsl_device_snapshot_close(device);
 
