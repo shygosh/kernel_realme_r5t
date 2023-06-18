01f30223dc236cea7c597c44feed16232e1fc212
Pzqqt <821026875@qq.com>
2023-06-18 16:08:54 +0800

    treewide: Add the necessary changes for KernelSU
    Wrap them with `if` macros.
    
    Signed-off-by: shygosh <mossynine@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1823,6 +1823,14 @@ out_ret:
 #endif /* CONFIG_OPPO_DISALLOW_KEY_INTERFACES */
 #endif /* CONFIG_VENDOR_EDIT */
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1836,6 +1844,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
