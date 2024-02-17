ec377fc156751ae970ef5e7ec2ab2ab0fde61c84
shygosh <mossynine@gmail.com>
2024-02-17 21:30:21 +0700

    qcacmn: Undefine QDF_ENABLE_TRACING
    Signed-off-by: shygosh <mossynine@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/qdf/linux/src/i_qdf_trace.h
+++ drivers/staging/qca-wifi-host-cmn/qdf/linux/src/i_qdf_trace.h
@@ -187,7 +187,7 @@ static inline void __qdf_trace_noop(QDF_MODULE_ID module, char *format, ...) { }
 #define QDF_TRACE_DEBUG_RL_NO_FL(params...) __qdf_trace_noop(params)
 #endif
 
-#define QDF_ENABLE_TRACING
+// #define QDF_ENABLE_TRACING
 #define qdf_scnprintf scnprintf
 
 #ifdef QDF_ENABLE_TRACING
